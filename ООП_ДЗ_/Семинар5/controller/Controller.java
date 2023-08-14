46181ed2614c0a2c965285d8bb8f1a88c5643215
Ivan Esin <ivan.esin.78@mail.ru>
2023-08-14 11:50:59 +0400

    пятый семинар

---
+++ ООП_ДЗ_/Семинар5/controller/Controller.java
@@ -0,0 +1,26 @@
+package ООП_ДЗ_.Семинар5.controller;
+
+import java.util.Calendar;
+
+import ООП_ДЗ_.Семинар5.model.data.Student;
+import ООП_ДЗ_.Семинар5.model.data.Teacher;
+import ООП_ДЗ_.Семинар5.model.service.DateService;
+import ООП_ДЗ_.Семинар5.model.service.UserService;
+
+public class Controller {
+
+  private DateService<Student> serviceStudent = new UserService<>();
+
+  public Student createNewStudent(String name, String lastName, Calendar birthDay, String group, String speciality,
+      double meanBall) {
+    return serviceStudent.createStudent(name, lastName, birthDay, group, speciality, meanBall);
+  }
+
+  private DateService<Teacher> serviceTeacher = new UserService<>();
+
+  public Teacher createNewTeacher(String name, String lastName, Calendar birthDay, String disciplines,
+      String department, double rating) {
+    return serviceTeacher.createTeacher(name, lastName, birthDay, disciplines, department, rating);
+  }
+
+}
---
+++ ООП_ДЗ_/Семинар5/main.java
@@ -0,0 +1,12 @@
+package ООП_ДЗ_.Семинар5;
+
+import ООП_ДЗ_.Семинар5.view.UserView;
+
+public class main {
+
+  public static void main(String[] args) {
+
+    UserView userView = new UserView();
+    userView.addUser();
+  }
+}
